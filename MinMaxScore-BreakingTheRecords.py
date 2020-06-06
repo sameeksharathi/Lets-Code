42c8e76fa74abfba3d3ffff02b07e90a67e65612
Sameeksha Rathi <43092943+sameeksharathi@users.noreply.github.com>
2020-06-07 00:20:10 +0530

    edited

--- MinMaxScore-BreakingTheRecords.py
+++ MinMaxScore-BreakingTheRecords.py
@@ -1,7 +1,9 @@
-'''
-Maria plays college basketball and wants to go pro. Each season she maintains a record of her play. She tabulates the number of times she breaks her season record for most points and least points in a game. Points scored in the first game establish her record for the season, and she begins counting from there.
+'''Maria plays college basketball and wants to go pro. Each season she maintains a record of her play. 
+She tabulates the number of times she breaks her season record for most points and least points in a game. 
+Points scored in the first game establish her record for the season, and she begins counting from there.
 
-For example, assume her scores for the season are represented in the array scores=[12,24,10,24]. Scores are in the same order as the games played. She would tabulate her results as follows:
+For example, assume her scores for the season are represented in the array scores=[12,24,10,24]. 
+Scores are in the same order as the games played. She would tabulate her results as follows:
 
                                  Count
 Game  Score  Minimum  Maximum   Min Max
@@ -9,12 +11,12 @@ Game  Score  Minimum  Maximum   Min Max
  1      24     12       24       0   1
  2      10     10       24       1   1
  3      24     10       24       1   1
+ 
 Given Maria's scores for a season, find and print the number of times she breaks her records for most and least points scored during the season.
 
 Input Format:
 The first line contains an integer n, the number of games.
-The second line contains n space-separated integers describing the respective values of score0, score1, score2-------,score(n-1).
-'''
+The second line contains n space-separated integers describing the respective values of score0, score1, score2-------,score(n-1).'''
 
 import math
 import os
