3aca1dc31ce566f52e20fc61af05e2c31bd152d3
Sameeksha Rathi <43092943+sameeksharathi@users.noreply.github.com>
2020-06-26 15:00:09 +0530

    Created

---
+++ Simple Array Sum/Solution.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+def simpleArraySum(ar):
+    sum = 0
+    for i in ar:
+        sum+=i
+    return sum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    ar_count = int(input())
+    ar = list(map(int, input().rstrip().split()))
+    result = simpleArraySum(ar)
+    fptr.write(str(result) + '\n')
+    fptr.close()
